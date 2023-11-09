9bd8d07eb464598e2246a0e0ab8678785de9f342
baristanunal <baristunal45@gmail.com>
2023-11-09 20:44:58 +0300

    Refactored file names & comments.

--- hello.js
+++
@@ -1,5 +0,0 @@
-// CS465 Homework #02
-// Mert Ünlü - 22003747
-// Barış Tan Ünal - 22003617
-
-console.log("hello");
---
+++ src/octopus.js
@@ -0,0 +1,11 @@
+/**
+ * Octopus JavaScript File for CS465, HW#02
+ * Bilkent University
+ * FALL 2023-2024
+ *
+ * @author  Mert Ünlü & Barış Tan Ünal
+ * @version 1.1
+ * @since   09-11-2023
+ */
+
+console.log("hello");
