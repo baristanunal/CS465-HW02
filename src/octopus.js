008f8900ccb44937a86383e8c2ccef1aa278f12f
Mert Ünlü <69141853+mertunlu@users.noreply.github.com>
2023-11-16 23:31:08 +0300

    jpeg added.

--- src/octopus.js
+++ src/octopus.js
@@ -631,6 +631,7 @@ window.onload = function init() {
 
     canvas = document.getElementById("gl-canvas");
 
+
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
